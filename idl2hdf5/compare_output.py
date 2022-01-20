53e2fd64b28c943307aba0341fdaa1aceb967dcc
Matthew West <m.t.west@exeter.ac.uk>
2022-01-20 12:12:46 +0000

    changed comparison script to check against "conservative variables" files in idl2hdf5 dir

--- idl2hdf5/compare_output.py
+++ idl2hdf5/compare_output.py
@@ -3,19 +3,14 @@ import numpy as np
 import pdb
 
 # Looping over time steps
-for i in np.arange(31):
-    fn_dac = "testdata.{0:04d}.h5".format(i)
+for i in np.arange(11):
+    fn_dac = "testdata_con.{0:04d}.h5".format(i)
 
     # For each reference file (contains multiple variables)
     with h5py.File(fn_dac, "r") as ref:
         for param in ref:
             ref_data = np.array(ref[param])
 
-            # adjust param tags to match sim produced files
-            if param.startswith('v'):
-                param = param.replace('v', 'm')
-            elif param == 'pr_p':
-                param = param.replace('pr', 'en')
             # define hdf5 filename of file to check
             if param == 'xgrid':
                 fn_h5 = "../Data/x.dac.0000.h5"
